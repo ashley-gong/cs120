4f557e1830cfd39e1c1d0fda24fb8a50aad2f675
ashley-gong <ashley.q.gong@gmail.com>
2023-09-23 17:00:32 -0400

    fixed select correctness

--- fall2023/psets/ps2/ps2.py
+++ fall2023/psets/ps2/ps2.py
@@ -4,7 +4,7 @@ class BinarySearchTree:
     # key: int
     # item: int
     # size: int
-    def __init__(self, debugger = None):
+    def __init__(self, debugger=None):
         self.left = None
         self.right = None
         self.key = None
@@ -14,9 +14,9 @@ class BinarySearchTree:
 
     @property
     def size(self):
-         return self._size
-       
-     # a setter function
+        return self._size
+
+    # a setter function
     @size.setter
     def size(self, a):
         debugger = self.debugger
@@ -25,11 +25,12 @@ class BinarySearchTree:
         self._size = a
 
     ####### Part a #######
-    '''
+    """
     Calculates the size of the tree
     returns the size at a given node
-    '''
-    def calculate_sizes(self, debugger = None):
+    """
+
+    def calculate_sizes(self, debugger=None):
         # Debugging code
         # No need to modify
         # Provides counts
@@ -46,29 +47,34 @@ class BinarySearchTree:
             self.size += self.left.calculate_sizes(debugger)
         return self.size
 
-    '''
+    """
     Select the ind-th key in the tree
     
     ind: a number between 0 and n-1 (the number of nodes/objects)
     returns BinarySearchTree/Node or None
-    '''
+    """
+
+    # error -> traversing on the right subtree and failing to change index (since only checking left)
     def select(self, ind):
         left_size = 0
         if self.left is not None:
             left_size = self.left.size
+        # print(left_size, ind)
         if ind == left_size:
             return self
         if left_size > ind and self.left is not None:
             return self.left.select(ind)
         if left_size < ind and self.right is not None:
-            return self.right.select(ind)
+            # fix is with right subtrees -> must subtract (left_size + 1) from index when checking on right side
+            return self.right.select(ind - left_size - 1)
         return None
 
-
-    '''
+    """
     Searches for a given key
     returns a pointer to the object with target key or None (Roughgarden)
-    '''
+    """
+
+    # Correct
     def search(self, key):
         if self is None:
             return None
@@ -79,19 +85,20 @@ class BinarySearchTree:
         elif self.left is not None:
             return self.left.search(key)
         return None
-    
 
-    '''
+    """
     Inserts a key into the tree
     key: the key for the new node; 
         ... this is NOT a BinarySearchTree/Node, the function creates one
     
     returns the original (top level) tree - allows for easy chaining in tests
-    '''
+    """
+
+    # too slow i think
     def insert(self, key):
         if self.key is None:
             self.key = key
-        elif self.key > key: 
+        elif self.key > key:
             if self.left is None:
                 self.left = BinarySearchTree(self.debugger)
             self.left.insert(key)
@@ -102,10 +109,9 @@ class BinarySearchTree:
         self.calculate_sizes()
         return self
 
-    
     ####### Part b #######
 
-    '''
+    """
     Performs a `direction`-rotate the `side`-child of (the root of) T (self)
     direction: "L" or "R" to indicate the rotation direction
     child_side: "L" or "R" which child of T to perform the rotate on
@@ -125,7 +131,8 @@ class BinarySearchTree:
         12
         /
        11 
-    '''
+    """
+
     def rotate(self, direction, child_side):
         # Your code goes here
         return self
@@ -133,7 +140,7 @@ class BinarySearchTree:
     def print_bst(self):
         if self.left is not None:
             self.left.print_bst()
-        print( self.key),
+        print(self.key),
         if self.right is not None:
             self.right.print_bst()
         return self
