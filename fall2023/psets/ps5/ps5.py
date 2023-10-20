8a9568be02e42436ad237dcd9ed0521785ca3f2a
ashley-gong <ashley.q.gong@gmail.com>
2023-10-19 22:35:30 -0400

    1a

--- fall2023/psets/ps5/ps5.py
+++ fall2023/psets/ps5/ps5.py
@@ -1,35 +1,42 @@
 from itertools import product, combinations
 
-'''
+"""
 Before you start: Read the README and the Graph implementation below.
-'''
+"""
+
 
 class Graph:
-    '''
+    """
     A graph data structure with number of nodes N, list of sets of edges, and a list of color labels.
 
     Nodes and colors are both 0-indexed.
     For a given node u, its edges are located at self.edges[u] and its color is self.color[u].
-    '''
+    """
 
     # Initializes the number of nodes, sets of edges for each node, and colors
-    def __init__(self, N, edges = None, colors = None):
+    def __init__(self, N, edges=None, colors=None):
         self.N = N
-        self.edges = [set(lst) for lst in edges] if edges is not None else [set() for _ in range(N)]
-        self.colors = [c for c in colors] if colors is not None else [None for _ in range(N)]
-    
+        self.edges = (
+            [set(lst) for lst in edges]
+            if edges is not None
+            else [set() for _ in range(N)]
+        )
+        self.colors = (
+            [c for c in colors] if colors is not None else [None for _ in range(N)]
+        )
+
     # Adds a node to the end of the list
     # Returns resulting graph
     def add_node(self):
         self.N += 1
         self.edges.append(set())
         return self
-    
+
     # Adds an undirected edge from u to v
     # Returns resulting graph
     def add_edge(self, u, v):
-        assert(v not in self.edges[u])
-        assert(u not in self.edges[v])
+        assert v not in self.edges[u]
+        assert u not in self.edges[v]
         self.edges[u].add(v)
         self.edges[v].add(u)
         return self
@@ -37,8 +44,8 @@ class Graph:
     # Removes the undirected edge from u to v
     # Returns resulting graph
     def remove_edge(self, u, v):
-        assert(v in self.edges[u])
-        assert(u in self.edges[v])
+        assert v in self.edges[u]
+        assert u in self.edges[v]
         self.edges[u].remove(v)
         self.edges[v].remove(u)
         return self
@@ -53,9 +60,9 @@ class Graph:
         return Graph(self.N, self.edges, self.colors)
 
     def clone_and_merge(self, g2, g1u, g2v):
-        '''
+        """
         DOES NOT COPY COLORS
-        '''
+        """
         g1 = self
         edges = g1.edges + [[v + g1.N for v in u_list] for u_list in g2.edges]
         g = Graph(g1.N + g2.N, edges)
@@ -67,7 +74,6 @@ class Graph:
     def is_graph_coloring_valid(self):
         for u in range(self.N):
             for v in self.edges[u]:
-
                 # Check if every one has a coloring
                 if self.colors[u] is None or self.colors[v] is None:
                     return False
@@ -75,21 +81,22 @@ class Graph:
                 # Make sure colors on each edge are different
                 if self.colors[u] == self.colors[v]:
                     return False
-        
+
         return True
 
-'''
+
+"""
     Introduction: We've implemented exhaustive search for you below.
 
     You don't need to implement any extra code for this part.
-'''
+"""
+
 
 # Given an instance of the Graph class G, exhaustively search for a k-coloring
 # Returns the coloring list if one exists, None otherwise.
 def exhaustive_search_coloring(G, k=3):
-
     # Iterate through every possible coloring of nodes
-    for coloring in product(range(0,k), repeat=G.N):
+    for coloring in product(range(0, k), repeat=G.N):
         G.colors = list(coloring)
         if G.is_graph_coloring_valid():
             return G.colors
@@ -99,14 +106,16 @@ def exhaustive_search_coloring(G, k=3):
     return None
 
 
-'''
+"""
     Part A: Implement two coloring via breadth-first search.
 
     Hint: You will need to adapt the given BFS pseudocode so that it works on all graphs,
     regardless of whether they are connected.
 
     When you're finished, check your work by running python3 -m ps5_color_tests 2.
-'''
+"""
+# from collections import deque
+
 
 # Given an instance of the Graph class G and a subset of precolored nodes,
 # Assigns precolored nodes to have color 2, and attempts to color the rest using colors 0 and 1.
@@ -126,25 +135,48 @@ def bfs_2_coloring(G, precolored_nodes=None):
 
         if len(precolored_nodes) == G.N:
             return G.colors
-    
-    # TODO: Complete this function by implementing two-coloring using the colors 0 and 1.
+
     # If there is no valid coloring, reset all the colors to None using G.reset_colors()
-    
+    vertex_order = [0]
+    for i in range(G.N):
+        for neighbor in G.edges[i]:
+            if neighbor not in vertex_order:
+                vertex_order.append(neighbor)
+
+    color = False
+    for i in range(len(vertex_order)):
+        for j in range(i):
+            if vertex_order[i] in G.edges[vertex_order[j]]:
+                color = not bool(G.colors[vertex_order[j]])
+                break
+        G.colors[vertex_order[i]] = int(color)
+        visited.add(vertex_order[i])
+
+    for i in range(G.N):
+        if i not in visited:
+            G.colors[i] = 0
+
+    if G.is_graph_coloring_valid():
+        return G.colors
+
     G.reset_colors()
     return None
 
-'''
+
+"""
     Part B: Implement is_independent_set.
-'''
+"""
+
 
 # Given an instance of the Graph class G and a subset of precolored nodes,
-# Checks if subset is an independent set in G 
+# Checks if subset is an independent set in G
 def is_independent_set(G, subset):
     # TODO: Complete this function
 
     return True
 
-'''
+
+"""
     Part C: Implement the 3-coloring algorithm from the sender receiver exercise.
     
     Make sure to call the bfs_2_coloring and is_independent_set functions that you already implemented!
@@ -162,7 +194,8 @@ def is_independent_set(G, subset):
 
     When you're finished, check your work by running python3 -m ps5_color_tests 3.
     Don't worry if some of your tests time out: that is expected.
-'''
+"""
+
 
 # Given an instance of the Graph class G (which has a subset of precolored nodes), searches for a 3 coloring
 # If successful, modifies G.colors and returns the coloring.
@@ -172,6 +205,7 @@ def iset_bfs_3_coloring(G):
 
     G.reset_colors()
     return None
+
 
 # Feel free to add miscellaneous tests below!
 if __name__ == "__main__":
